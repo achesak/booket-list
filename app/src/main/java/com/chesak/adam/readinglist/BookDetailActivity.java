3bbcb26658c6dee99f0a5b26ba991baa50901d0b
achesak <achesak@yahoo.com>
2017-05-27 12:08:32 -0400

    Remove unused imports

--- app/src/main/java/com/chesak/adam/readinglist/BookDetailActivity.java
+++ app/src/main/java/com/chesak/adam/readinglist/BookDetailActivity.java
@@ -7,8 +7,6 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,8 +15,6 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.Locale;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * Shows the book details
